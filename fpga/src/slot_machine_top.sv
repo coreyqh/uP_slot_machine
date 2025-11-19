a9e9fc1c73591cf50ce6aec870e04ed09f4088ca
SadhviNarayanan <146152067+SadhviNarayanan@users.noreply.github.com>
2025-11-18 17:06:40 -0800

    Refactor slot_machine_top module for PLL integration

--- fpga/src/slot_machine_top.sv
+++ fpga/src/slot_machine_top.sv
@@ -1,18 +1,21 @@
-module slot_machine_top (input  logic clk, 
+module slot_machine_top (//input  logic clk, 
                          input  logic reset_n,
 
-                         input  logic sclk, 
-                         input  logic copi, 
-                         input  logic cs, 
-                         output logic sdo, 
+                         //input  logic sclk, 
+                         //input  logic copi, 
+                         //input  logic cs, 
+                         //output logic sdo, 
 
                          output logic hsync, 
                          output logic vsync,
                          output logic [2:0] vga_rgb,
                          output logic done,
+						 output logic debug_pll_clk,
+						 output logic [2:0] state_led
                         
-                         output logic [4:0] select,
-                         output logic [6:0] seven_segment_output);
+                         //output logic [4:0] select,
+                         //output logic [6:0] seven_segment_output 
+						 );
 
     logic [10:0] hcount;
     logic [9:0] vcount;
@@ -24,7 +27,9 @@ module slot_machine_top (input  logic clk,
     logic [11:0] total_credits;
     logic        is_total;
     logic        active_video;
+	logic pll_clk_internal, pll_clock;
 
+    /*
     spi_data_extract spi_data_extract (
         .sclk          (sclk),
         .reset_n       (reset_n),
@@ -40,30 +45,49 @@ module slot_machine_top (input  logic clk,
         .total_credits (total_credits),
         .is_total      (is_total)
     );
-    
+	*/
+	
+     pll_clock #(
+		.CLKHF_DIV("0b00"),
+        .DIVR("0"),
+		.DIVF("16"),
+		.DIVQ("5")
+    ) PLL_CLK (
+        .rst_n       (reset_n),
+        .clk_internal(pll_clk_internal),
+        .clk_external(debug_pll_clk),
+        .clk_HSOSC   (debug_HSOSC_clk),
+        .locked      (pll_lock)
+    );
+
     vga_controller vga_controller (
-        .clk           (clk),
-        .reset_n       (reset_n),
+        .clk           (pll_clk_internal),
+        .reset_n       (reset_n & pll_lock),
         .hsync         (hsync),
         .vsync         (vsync),
         .hcount        (hcount),
         .vcount        (vcount),
         .active_video  (active_video)
     );
+	assign reel1_idx = 2;
+	assign reel2_idx = 0;
+	assign reel3_idx = 1;
+	assign start_spin = 1;
 
-    memory_controller u_memory_controller (
-        .clk              (clk),
-        .reset_n          (reset_n),
+    memory_controller u_memory_controller ( 
+        .clk              (pll_clk_internal),
+        .reset_n          (reset_n & pll_lock),
         .hcount           (hcount),
         .vcount           (vcount),
         .vsync            (vsync),
-        .active_video     (active_video);
+        .active_video     (active_video),
         .final1_sprite    (reel1_idx[2:0]),  // assuming 3-bit sprite IDs
         .final2_sprite    (reel2_idx[2:0]),
         .final3_sprite    (reel3_idx[2:0]),
         .start_spin       (start_spin),
         .pixel_rgb        (vga_rgb),
-        .done             (done) // so far only done for hwen finished spinning since havent done update points stuff
+        .done             (done), // so far only done for hwen finished spinning since havent done update points stuff
+		.state_led		  (state_led)
     );
 
     // seven segment display

3ec1154e9379ea73fb80dcca6e5dda618f90b523
SadhviNarayanan <146152067+SadhviNarayanan@users.noreply.github.com>
2025-11-20 06:54:32 -0800

    Refactor output signal and modify assignment logic
    Updated output signal from 'rgb' to 'dout' and modified the assignment logic. Commented out the clocked assignment for 'dout'.

--- fpga/src/rom_block2.sv
+++ fpga/src/rom_block2.sv
@@ -4,23 +4,22 @@ module rom_block2 #(
 ) (
     input  logic clk,
     input  logic [7:0] address,
-    output logic [15:0] rgb
+    output logic [15:0] dout
 );
     localparam SPRITE_WIDTH = 64;
     localparam DATA_WIDTH = 16;
     localparam TOTAL_WORDS = 256;
     localparam ADDRESS_WIDTH = 8;
     
-    (* syn_ramstyle = "EBR" *)
-    (* syn_preserve = 1 *)
     logic [DATA_WIDTH-1:0] single_bram1 [0:TOTAL_WORDS-1];
     
     initial begin 
         $readmemh(text_file, single_bram1);
     end
-    
-    always_ff @(posedge clk) begin 
-        rgb <= single_bram1[address]; 
-    end
-endmodule
 
+    assign dout = single_bram1[address]; 
+    
+    // always_ff @(posedge clk) begin 
+    //     dout <= single_bram1[address]; 
+    // end
+endmodule

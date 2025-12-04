199bf57c8faa182ed83267c64893cc4b2231c808
SadhviNarayanan <146152067+SadhviNarayanan@users.noreply.github.com>
2025-12-04 07:07:44 -0800

    Update credit_controller.sv

--- fpga/src/credit_controller.sv
+++ fpga/src/credit_controller.sv
@@ -1,9 +1,8 @@
-// E155, Lab 2 - Code to display two numbers on a time-multiplexed dual segment display
+// E155, Enable 1/5 of the seven segment displays based on clk frequency for time multiplexing of the seven segment display module
 
 // Name: Sadhvi Narayanan
 // Email: sanarayanan@g.hmc.edu
-// Date: 09/03/2025
-
+// Date: 12/04/2025
 
 module credit_controller(
 	input logic clk,
@@ -17,7 +16,7 @@ module credit_controller(
     output logic [6:0] seg
 );
 
-
+  // internal signals
   logic int_osc;
   logic [23:0] counter;
   logic clk_signal;
@@ -27,30 +26,32 @@ module credit_controller(
   // toggles the clock at 60Hz - frequency where humans cannot see flickers 
   select_toggle select_toggle_controller(clk, reset_n, freq_counter);
   
+  // seven segment display output
   seven_segment seven_segment_decoder2(s, seg);
 
 
   always_comb begin
        if (freq_counter == 0) begin
 		   // determines which pnp transistor to provide a load to
-			enable_sel = 5'b01111; // enable first 7-seg
-			s = won_amt1; // chooses first DIP switch
+			enable_sel = 5'b01111; // enable 4th 7-seg
+			s = won_amt1; // chooses 4th DIP switch
        end else if (freq_counter == 1) begin
 		    // determines which pnp transistor to provide a load to
-			enable_sel = 5'b10111; // enable first 7-seg
-			s = won_amt2; // chooses first DIP switch 
+			enable_sel = 5'b10111; // enable 5th 7-seg
+			s = won_amt2; // chooses 5th DIP switch 
 	   end else if (freq_counter == 2) begin
 		    // determines which pnp transistor to provide a load to
-			enable_sel = 5'b11011; // enable first 7-seg
-			s = credit_amt1; // chooses first DIP switch
+			enable_sel = 5'b11011; // enable 1st 7-seg
+			s = credit_amt1; // chooses 1st DIP switch
 	   end else if (freq_counter == 3) begin
 		    // determines which pnp transistor to provide a load to
-			enable_sel = 5'b11101; // enable first 7-seg
-			s = credit_amt2; // chooses first DIP switch
+			enable_sel = 5'b11101; // enable 2nd 7-seg
+			s = credit_amt2; // chooses 2nd DIP switch
 	   end else begin
 		    // determines which pnp transistor to provide a load to
-			enable_sel = 5'b11110; // enable first 7-seg
-			s = credit_amt3; // chooses first DIP switch
+			enable_sel = 5'b11110; // enable 3rd 7-seg
+			s = credit_amt3; // chooses 3rd DIP switch
 	   end	
    end
  endmodule
+
